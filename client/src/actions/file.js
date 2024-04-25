cec9a364b86f2a035d67df01c25b8647d4c07333
Владислава Зиновейкина <107256699+VladislavaZinoveikina@users.noreply.github.com>
2024-04-25 17:13:02 +0200

    createDir, popupDisplay

--- client/src/actions/file.js
+++ client/src/actions/file.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { setFiles } from "../reducers/fileReducer";
+import { addFile, setFiles } from "../reducers/fileReducer";
 
 export function getFiles(dirId) {
     return async dispatch => {
@@ -8,6 +8,23 @@ export function getFiles(dirId) {
                 headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
             });
             dispatch(setFiles(responce.data))
+        } catch (e) {
+            alert(e.responce.data.message)
+        }
+    };
+};
+
+export function createDir(dirId, name) {
+    return async dispatch => {
+        try {
+            const responce = await axios.post(`http://localhost:3000/api/files`, {
+            name,
+            parent: dirId,
+            type: 'dir'
+        }, {
+                headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
+            });
+            dispatch(addFile(responce.data))
         } catch (e) {
             alert(e.responce.data.message)
         }
--- client/src/components/disk/Disk.jsx
+++ client/src/components/disk/Disk.jsx
@@ -1,8 +1,10 @@
 import React, {useEffect} from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getFiles } from "../../actions/file";
+import { createDir, getFiles } from "../../actions/file";
 import FileList from "./fileList/FileList";
 import './disk.css';
+import Popup from "./Popup";
+import { setPopupDisplay } from "../../reducers/fileReducer";
 
 const Disk = () => {
     const dispatch = useDispatch();
@@ -12,13 +14,18 @@ const Disk = () => {
         dispatch(getFiles(currentDir))
     }, [currentDir]);
 
+    function showPopupHandler() {
+        dispatch(setPopupDisplay('flex'))
+    }
+
     return (
         <div className="disk">
             <div className="disk__btns" id="btns">
                 <button className="disk__back">Go back</button>
-                <button className="disk__create" id="create">New folder</button>
+                <button className="disk__create" id="create" onClick={() => showPopupHandler()}>Create new folder</button>
             </div>
             <FileList/>
+            <Popup/>
         </div>
     );
 };
