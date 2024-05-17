495f69d98956b650040a505dcf6a1468e1769b6e
Владислава Зиновейкина <107256699+VladislavaZinoveikina@users.noreply.github.com>
2024-05-17 09:37:03 +0200

    add uploder

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,14 +1,17 @@
 import {applyMiddleware, combineReducers} from "redux";
 import {compose} from "redux";
-import {thunk} from "redux-thunk";
+import thunk from "redux-thunk";
 import userReducer from "./userReducer";
 import fileReducer from "./fileReducer";
 import { legacy_createStore as createStore} from 'redux';
+import uploadReducer from "./uploadReducer"
+
 
 
 const rootReducer = combineReducers({
     user: userReducer,
     files: fileReducer,
+    upload: uploadReducer
 });
 
 export const store = createStore(rootReducer, compose(applyMiddleware(thunk)))
